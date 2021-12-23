e5d6d9c6be6f21405eb83d34b0345bcfdab60fdb
AmirhHosein Rastgar <me@amirrastgar.ir>
2021-12-23 17:01:18 +0330

    c++

---
+++ Draw a triangle.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int a,b,c;
+	
+	cout<<"Enter the triangle chord ";
+	cin>>a;
+	
+	cout<<"Enter the sides of the triangle ";
+	cin>>b>>c;
+	
+	if (a<b+c1)
+	{
+		cout<<"yes";
+	}
+	else
+	{
+		cout<<"no";
+	}
+	
+	return 0;
+}
