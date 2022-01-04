959fa0307e6b566b1f58ec9fde501e68cd940d16
AmirhHosein Rastgar <me@amirrastgar.ir>
2022-01-04 11:50:04 +0330

    .

---
+++ Pow.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+	int x;
+	
+	cout<<"Enter a number ";
+	cin>>x;
+	
+	cout<<pow(x,1)<<endl<<pow(x,2)<<endl<<pow(x,3);
+	
+	return 0;
+}
